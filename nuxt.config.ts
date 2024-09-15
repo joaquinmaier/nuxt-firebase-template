19a82afb4fe1614d90650075eb6212682f4f6c2a
joaquinmaier <51100746+joaquinmaier@users.noreply.github.com>
2024-09-15 00:20:09 -0300

    Updated packages & created nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   modules: [
     "@pinia/nuxt"
-  ]
+  ],
+
+  compatibilityDate: "2024-09-14"
 })
